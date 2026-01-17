d9e616aaf0d883bb7fc5393116c4bd275b271b71
HyeJinRyoo <hjryoo31@gmail.com>
2026-01-17 22:17:20 +0900

    update Log Message + AggregatedLogMetric

--- src/main/java/org/example/model/LogMessage.java
+++ src/main/java/org/example/model/LogMessage.java
@@ -3,11 +3,11 @@ package org.example.model;
 import lombok.Builder;
 
 @Builder
-public class LogMessage {
-    private String timestamp;   // 발생 시간 (ISO-8601)
-    private String logLevel;    // INFO, WARN, ERROR
-    private String serviceName; // payment-service, order-service 등
-    private String message;     // 로그 내용
-    private String userId;      // 사용자 ID (분석용)
-    private long responseTime;  // 응답 속도 (ms)
-}
+public record LogMessage(
+        String timestamp,
+        String logLevel,
+        String serviceName,
+        String message,
+        String userId,
+        long responseTime
+) {}
